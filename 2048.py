b15631e900bb0f70fa55babf3f782158f4876371
MahadiAnkon <ankon1807075@stud.kuet.ac.bd>
2023-06-09 15:05:46 +0600

    Ui Updated

--- 2048.py
+++ 2048.py
@@ -2,8 +2,6 @@ import sys
 import pygame
 import random
 import math
-import subprocess
-
 from colors import *
 
 pygame.init()
@@ -20,14 +18,13 @@ grid_padding = 8
 board = [[0 for _ in range(4)] for _ in range(4)]
 score = 0
 direction = ""
-
+game_state = "menu"
 timer_duration = 2 * 60 + 30
-start_time = pygame.time.get_ticks()
 lives = 1
 moves_without_change = 0
 max_moves_without_change = 3
 move_time_limit = 5
-last_move_time = pygame.time.get_ticks()
+
 
 def draw_score_board(remaining_time, move_time_limit, lives):
     pygame.draw.rect(sc, (230, 230, 230), [0, 0, wd, 100])
@@ -105,8 +102,102 @@ def draw_new(board):
                     board[row][col] = 2
                 break
     return board
+def mainmenu():
+    pygame.init()
+    pygame.display.set_caption("Main Menu")
+
+    clock = pygame.time.Clock()
+    font = pygame.font.Font(None, 32)
+    button_width, button_height = 200, 50
+
+    start_game_x = (wd - button_width) // 2
+    start_game_y = (he - button_height) // 2 + 80
+    two_minutes_x = (wd - button_width) // 2
+    two_minutes_y = (he - button_height) // 2 + 160
+
+    start_game_button = pygame.Rect(start_game_x, start_game_y, button_width, button_height)
+    two_minutes_button = pygame.Rect(two_minutes_x, two_minutes_y, button_width, button_height)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                if start_game_button.collidepoint(mouse_pos):
+                    return "gameplay"
+                    
+                elif two_minutes_button.collidepoint(mouse_pos):
+                    pygame.quit()
+                    sys.exit()
+
+        sc.fill(WHITISH)
+
+        if start_game_button.collidepoint(pygame.mouse.get_pos()):
+            pygame.draw.rect(sc, CELL_COLORS[8], start_game_button)
+        else:
+            pygame.draw.rect(sc, CELL_COLORS[16], start_game_button)
+        if two_minutes_button.collidepoint(pygame.mouse.get_pos()):
+            pygame.draw.rect(sc, GRID_COLOR, two_minutes_button)
+        else:
+            pygame.draw.rect(sc, GRAYISH, two_minutes_button)
+
+        start_game_text = font.render("Start Game", True, (255, 255, 255))
+        start_game_rect = start_game_text.get_rect(center=start_game_button.center)
+        sc.blit(start_game_text, start_game_rect)
+
+        two_minutes_text = font.render("Exit", True, (255, 255, 255))
+        two_minutes_rect = two_minutes_text.get_rect(center=two_minutes_button.center)
+        sc.blit(two_minutes_text, two_minutes_rect)
+
+        pygame.display.flip()
+        clock.tick(60)
 
 
+def mm():
+    wd, he = 400, 500
+    sc = pygame.display.set_mode((wd, he))
+    pygame.display.set_caption("Game Over")
+    if len(sys.argv) > 1:
+        score = int(sys.argv[1])
+
+
+    clock = pygame.time.Clock()
+    font = pygame.font.Font(None, 32)
+    button_width, button_height = 200, 50
+
+    game_over_x = (wd - button_width) // 2
+    game_over_y = (he - button_height) // 2 - 50
+    main_menu_x = (wd - button_width) // 2
+    main_menu_y = (he - button_height) // 2 + 50
+
+    game_over_text = font.render("Game Over ", True, (255, 255, 255))
+    game_over_rect = game_over_text.get_rect(center=(wd // 2, game_over_y))
+
+    game_over_button = pygame.Rect(main_menu_x, main_menu_y, button_width, button_height)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                if game_over_button.collidepoint(mouse_pos):
+                    return "menu"
+
+        sc.fill(WHITISH)
+        
+        sc.blit(game_over_text, game_over_rect)
+
+        pygame.draw.rect(sc, (255, 0, 0), game_over_button)
+
+        main_menu_text = font.render("Go to Main Menu", True, (255, 255, 255))
+        main_menu_rect = main_menu_text.get_rect(center=game_over_button.center)
+        sc.blit(main_menu_text, main_menu_rect)
+
+        pygame.display.flip()
+        clock.tick(60)
 def stack():
     global board
     new_matrix = [[0] * 4 for _ in range(4)]
@@ -196,46 +287,58 @@ running = True
 draw_new_board = True
 
 while running:
-    remaining_time = max(timer_duration - (pygame.time.get_ticks() - start_time) // 1000, 0)
-
-    if moves_without_change >= max_moves_without_change or lives == 0 or remaining_time == 0:
-        subprocess.Popen(["python", "gameover.py"])
-        pygame.display.flip()
-        pygame.time.wait(1000)
-        pygame.quit()
-        sys.exit()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                direction = "left"
-            elif event.key == pygame.K_RIGHT:
-                direction = "right"
-            elif event.key == pygame.K_DOWN:
-                direction = "down"
-            elif event.key == pygame.K_UP:
-                direction = "up"
-
-    current_time = pygame.time.get_ticks()
-    elapsed_time = (current_time - last_move_time) // 1000
-
-    if draw_new_board or init_count < 2:
-        board = draw_new(board)
-        draw_new_board = False
-        init_count += 1
-    if direction != "":
-        take_turn(direction=direction)
+    if(game_state=="menu"):
+        a = mainmenu()
+        print("kuta")
+        start_time = pygame.time.get_ticks()
+        last_move_time = pygame.time.get_ticks()
+        board = [[0 for _ in range(4)] for _ in range(4)]
         draw_new_board = True
-        direction = ""
-        last_move_time = pygame.time.get_ticks()
-        moves_without_change = 0
-    if elapsed_time >= move_time_limit:
-        lives -= 1
-        last_move_time = pygame.time.get_ticks()
-    draw_board()
-    draw_score_board(remaining_time, elapsed_time, lives)
-    draw_tiles(board)
-    pygame.display.flip()
-    timer.tick(fps)
+        init_count=0
+        score = 0
+        lives = 1
+        game_state = a
+    else:
+        remaining_time = max(timer_duration - (pygame.time.get_ticks() - start_time) // 1000, 0)
+        if lives == 0 or remaining_time == 0:
+            a = mm()
+            game_state = a
+
+         
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    direction = "left"
+                elif event.key == pygame.K_RIGHT:
+                    direction = "right"
+                elif event.key == pygame.K_DOWN:
+                    direction = "down"
+                elif event.key == pygame.K_UP:
+                    direction = "up"
+
+        current_time = pygame.time.get_ticks()
+        elapsed_time = (current_time - last_move_time) // 1000
+
+        if draw_new_board or init_count < 2:
+            board = draw_new(board)
+            draw_new_board = False
+            init_count += 1
+        if direction != "":
+            take_turn(direction=direction)
+            draw_new_board = True
+            direction = ""
+            last_move_time = pygame.time.get_ticks()
+            moves_without_change = 0
+        if elapsed_time >= move_time_limit:
+            lives -= 1
+            last_move_time = pygame.time.get_ticks()
+        draw_board()
+        draw_score_board(remaining_time, elapsed_time, lives)
+        draw_tiles(board)
+        pygame.display.flip()
+        timer.tick(fps)
+
+
